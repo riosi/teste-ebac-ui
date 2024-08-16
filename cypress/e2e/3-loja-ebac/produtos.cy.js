91bdc432670e26bc01172f8d0b2b74e1cec404aa
Ingryd Rios <riosingryd@gmail.com>
2024-08-16 12:10:36 -0300

    listas

---
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -0,0 +1,31 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade: Produtos', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+
+    afterEach(() => {
+        cy.get('#tab-title-description > a').should('contain', 'Descrição')
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('.products > .row')
+        .contains('Ariel Roll Sleeve Sweatshirt')
+        .click()
+    });
+
+    it('Deve selecionar o primeiro produto da lista', () => {
+        cy.get('.product-block').first().click()
+    });
+
+    it('Deve selecionar o último produto da lista', () => {
+        cy.get('.product-block').last().click()
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('.product-block').eq(2).click()
+    });
+
+});
